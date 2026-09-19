5b142415ca17ff00905e9aced57c9673b431768b
ArSeNyaChka <arsentereshchenko2007@gmail.com>
2026-09-19 23:42:28 +0700

    idc meow

--- bebebe.cpp
+++ bebebe.cpp
@@ -1,69 +1,15 @@
-#include <cmath>
-#include <vector>
+#include <fstream>
+#include <string>
 
 
-class Shape {
-public:
-    // TODO: объявите чисто виртуальные методы area() и perimeter()
-    virtual double area() const = 0;
-    virtual double perimeter() const = 0;
-    virtual ~Shape() = default;
-};
+int main(int argc, char* argv[]) {
+    std::ofstream f("out.cpp");
+    f << "bool isChetnoe(int a) {";
 
-// TODO: реализуйте классы и методы
-class Rectangle : public Shape {
-private:
-    double width;
-    double height;
-public:
-    Rectangle(double width, double height) : width(width), height(height) {}
-    double area() const override {
-        return width * height;
-    }
-    double perimeter() const override {
-        return 2 * (width + height);
-    }
-};
+    for (int i = 0; i < std::stoi(argv[1]); i++)
+        f << "\n    if (a == " + std::to_string(i) + ")\n       return " + (i % 2 == 0 ? "true" : "false") + ";";
 
-class Triangle : public Shape {
-private:
-    double a, b, c;
-public:
-    Triangle(double a, double b, double c) : a(a), b(b), c(c) {}
-    double perimeter() const override {
-        return a + b + c;
-    }
-    double area() const override {
-        double s = perimeter() / 2;
-        return sqrt(s * (s - a) * (s - b) * (s - c));
-    }
-};
+    f << "\n}";
 
-
-bool operator<(const Shape& lhs, const Shape& rhs) {
-    return lhs.area() < rhs.area();
-}
-
-bool operator>(const Shape& lhs, const Shape& rhs) {
-    return lhs.area() > rhs.area();
-}
-
-bool operator<=(const Shape& lhs, const Shape& rhs) {
-    return lhs.area() <= rhs.area();
-}
-
-bool operator>=(const Shape& lhs, const Shape& rhs) {
-    return lhs.area() >= rhs.area();
-}
-
-std::vector<Shape*> sortShapes(const std::vector<Shape*>& shapes) {
-    std::vector<Shape*> sortedShapes = shapes;
-    for (size_t i = 0; i < sortedShapes.size() - 1; ++i) {
-        for (size_t j = 0; j < sortedShapes.size() - i - 1; ++j) {
-            if (*sortedShapes[j] > *sortedShapes[j + 1]) {
-                std::swap(sortedShapes[j], sortedShapes[j + 1]);
-            }
-        }
-    }
-    return sortedShapes;
+    return 0;
 }
